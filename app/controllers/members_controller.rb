81e9c3811bbdbd408f22f49cae8c728f6b5d2dca
Takahashi Nonoka <ne300229@senshu-u.jp>
2019-11-07 04:19:01 +0900

    ページネーションを実装（9.4）

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,10 +4,12 @@ class MembersController < ApplicationController
   # 会員一覧
   def index
 	  @members = Member.order("number")
-	end
+      .page(params[:page]).per(15)
+  end
 
 	def search
 		@members = Member.search(params[:q])
+		  .page(params[:page]).per(15)
 		render "index"
 	end
 
