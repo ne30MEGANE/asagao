53164ad86362ed3f94fb9c8d6138dfde91fa02fb
Takahashi Nonoka <ne300229@senshu-u.jp>
2019-11-14 13:50:07 +0900

    14.4 おわり

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -56,4 +56,17 @@ class EntriesController < ApplicationController
     current_member.voted_entries << @entry
     redirect_to @entry, notice: "投票しました。"
   end
+
+  # 投票削除
+  def unlike
+    current_member.voted_entries.destroy(Entry.find(params[:id]))
+    redirect_to :voted_entries, notice: "削除しました。"
+  end
+
+  # 投票した記事
+  def voted
+    @entries = current_member.voted_entries.published
+      .order("votes.created_at DESC")
+      .page(params[:page]).per(15)
+  end
 end
