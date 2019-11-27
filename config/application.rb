d448f86af6529b71a4570d0e32ca9c2acdda3af8
Takahashi Nonoka <ne300229@senshu-u.jp>
2019-11-27 20:39:04 +0900

    13.2 おわり

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Asagao
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
 
-    config.action_controller.permit_all_parameters = false
+    config.action_controller.permit_all_parameters = true
 
     config.exceptions_app = ->(env) do
       ErrorsController.action(:show).call(env)
